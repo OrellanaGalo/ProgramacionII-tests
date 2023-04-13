5ce7d33fbf7931614f35bc0e5d1acbe712ee2735
OrellanaGalo <Galoorellana2@hotmail.com>
2023-04-12 22:52:34 -0300

    Commit 0.2
    > Agregue los metodos y test que quedaban, le alumne que use de prueba es Agustina Bover

--- PII2023-Parcial1-tests-main/ComparacionDeMetodosTest.java
+++ PII2023-Parcial1-tests-main/ComparacionDeMetodosTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.Test;
@@ -22,5 +23,33 @@ public class ComparacionDeMetodosTest{
         } catch (MetodosGalo.ArregloFueraDeRangoException e){
             fail("No se esperaba una excepcion: " + e.getMessage());
         }
+    }
+
+    @Test
+    public void testComparacionDeMetodosBuscar(){
+        int[] arreglo = {1, 2, 3, 4, 5};
+        int numero = 3;
+
+        int resultado_esperado = 2;
+
+        int resultado_uno = MetodosGalo.buscar(arreglo, numero);
+        int resultado_dos = MetodosAlumno.buscar(arreglo, numero);
+
+        assertEquals(resultado_esperado, resultado_uno);
+        assertEquals(resultado_esperado, resultado_dos);
+    }
+
+    @Test
+    public void testComparacionDeMetodosContenido(){
+        int[] arreglo_uno = {1, 2, 3, 4, 5};
+        int[] arreglo_dos = {3, 4};
+
+        boolean resultado_esperado = true;
+
+        boolean resultado_uno = MetodosGalo.esContenido(arreglo_uno, arreglo_dos);
+        boolean resultado_dos = MetodosAlumno.contenido(arreglo_uno, arreglo_dos);
+
+        assertEquals(resultado_esperado, resultado_uno);
+        assertEquals(resultado_esperado, resultado_dos);
     }
 }
--- PII2023-Parcial1-tests-main/MetodosAlumno.java
+++ PII2023-Parcial1-tests-main/MetodosAlumno.java
@@ -1,7 +1,9 @@
+/**
+ * En esta clase se insertan los metodos creados por los alumnos para comparar si retornan bien los resultados :)
+ */
 public class MetodosAlumno {
 
     /**
-     * AGUSTINA BOVER
      * Esta funcion se encarga de agregar un numero en la posicion indicada del arreglo
      * @param array es el arreglo ingresado por el usuario
      * Pre: arreglo no nulo de numeros enteros
@@ -36,5 +38,52 @@ public class MetodosAlumno {
             }
         }
         return arregloNuevo;
+    }
+
+    /**
+     *  Esta funcion se encarga de bbuscar un numero dentro de un arreglo y devolver la posicion en la que se encuentra
+     * @param array es el arreglo ingresado por el usuario
+     * Pre: arreglo no nulo de numeros enteros
+     * @param numero es el numero que hay que encontrar
+     * Pre: nro entero
+     * Post: Devolver la posicion del nro buscado
+     */
+    public static int buscar(int[] array, int numero) {
+        int posicion=-1;
+        for(int i=0;i< array.length;i++){
+            if (array[i]==numero){
+                posicion=i;
+            }
+        }
+        return posicion;
+    }
+
+    /**
+     *  Esta funcion se encarga de verificar si los valores de un arreglo se encuentran dentro de otro
+     * @param array1 es el arreglo ingresado por el usuario
+     * Pre: arreglo no nulo de numeros enteros
+     * @param array2 es el arreglo ingresado por el usuario
+     * Pre: arreglo no nulo de numeros enteros
+     * Post: Devolver true/false si el arreglo2 se encuentra en el arreglo1
+     */
+    public static boolean contenido(int[] array1, int[] array2) {
+        int valoresComunes=0;
+        int cantidadValores=0;
+        for(int i = 0; i<array2.length; i++){
+            cantidadValores=cantidadValores+1;
+        }
+        int n=1;
+        int j=0;
+        for (int i=0; i<array1.length*cantidadValores; i++){
+            if (array1[i]==array2[j]){
+                valoresComunes=valoresComunes+1;
+            }
+            if (i==array1.length*n){
+                n++;
+                j++;
+            }
+        }
+        boolean resultado=valoresComunes==array2.length;
+        return resultado;
     }
 }
